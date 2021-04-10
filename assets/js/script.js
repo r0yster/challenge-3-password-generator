6c047341b8445d5d450037b827576096652b06b4
royster <rcantu786@gmail.com>
2021-04-10 10:34:55 -0500

    fixed uppercase bug resolved edge cases

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,48 @@
+// string variables holding characters to use for password generation
 const lowerChars = "abcdefghijklmnopqrstuvwxyz";
 const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numericChars = "0123456789";
 const specialChars = "!@#$%^&*(){}[]=<>/,."
-var validChars = "";
 
+// password object initialization
 var pwCriteria = {
   length: 0,
   lowercase: false,
   uppercase: false,
   numeric: false,
-  special: false
+  special: false,
+  validChars: "",
+  reset: function() {
+    this.length = 0;
+    this.lowercase = false;
+    this.uppercase = false;
+    this.numeric = false;
+    this.special = false;
+    this.validChars = "";
+  }
 }
 
 // Assignment code here
 function generatePassword() {
-
+  // reset values to ensure clean run
+  pwCriteria.reset();
+  // initialize return value
   var genPassword = "";
 
+  // funtion call to gatherCriteria()
   alert("Please select your criteria...");
   gatherCriteria();
 
   for (var i = 0; i < pwCriteria.length; i++) {
-    genPassword += validChars[Math.floor(Math.random() * validChars.length)];
+    genPassword += pwCriteria.validChars[Math.floor(Math.random() * pwCriteria.validChars.length)];
   }
 
   return genPassword;
-}
+};
 
-// gatherCriteria function
+// gather user criteria
 function gatherCriteria(){
-
+  // check valid length, leaving when pw.Criteria length is within 8 - 128 
   while (pwCriteria.length < 8 || pwCriteria.length > 128) {
     pwCriteria.length = prompt("Please enter length of password: [ 8 - 128 ]");
     if (pwCriteria.length < 8 || pwCriteria.length > 128) {
@@ -37,17 +50,26 @@ function gatherCriteria(){
     }
   }
 
+  // prompt user for password criteria (lower, upper, numeric, special) using confirm
+  // if confirm === true, append confirmed criteria string to validChars string
   pwCriteria.lowercase = confirm("Do you want lowercase characters?");
-  if (pwCriteria.lowercase) validChars += lowerChars; 
+  if (pwCriteria.lowercase) pwCriteria.validChars += lowerChars; 
 
-  pwCriteria.lowercase = confirm("Do you want uppercase characters?");
-  if (pwCriteria.lowercase) validChars += upperChars; 
+  pwCriteria.uppercase = confirm("Do you want uppercase characters?");
+  if (pwCriteria.uppercase) pwCriteria.validChars += upperChars; 
 
   pwCriteria.numeric = confirm("Do you want numeric characters?");
-  if (pwCriteria.numeric) validChars += numericChars; 
+  if (pwCriteria.numeric) pwCriteria.validChars += numericChars; 
 
   pwCriteria.special = confirm("Do you want special characters?");
-  if (pwCriteria.special) validChars += specialChars; 
+  if (pwCriteria.special) pwCriteria.validChars += specialChars;
+
+  // no criteria selected check
+  if (!pwCriteria.lowercase && !pwCriteria.uppercase && !pwCriteria.numeric && !pwCriteria.special) {
+    alert("No criteria selected!");
+    pwCriteria.reset();
+    gatherCriteria();
+  }
 
 };
 
